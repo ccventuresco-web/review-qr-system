5ace18ce8619ddb59027a2cc1b0dee736900b718
ccventuresco-web <ccventuresco@gmail.com>
2026-04-29 03:01:11 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const frontendPath = path.join(__dirname, "..", "frontend");
 app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(frontendPath));
 
 function getPublicBaseUrl(req) {
